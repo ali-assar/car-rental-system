7d6a558d65b628939981f12c139f352a3aa66ac1
Ali-Assar <ali.assar@skiff.com>
2024-01-10 11:40:01 +0330

    user fetching reservation is tested

--- api/reservation_handler_test.go
+++ api/reservation_handler_test.go
@@ -15,6 +15,43 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func TestUserGetReservation(t *testing.T) {
+	db := setup(t)
+	defer db.tearDown(t)
+
+	var (
+		//nonAuthUser        = fixtures.AddUser(db.Store, "james", "wrong", false)
+		user               = fixtures.AddUser(db.Store, "james", "foo", false)
+		agency             = fixtures.AddAgency(db.Store, "voom voom", "rome", 3, nil)
+		car                = fixtures.AddCar(db.Store, "sport", "petrol", "BMW", 2017, 100, agency.ID)
+		from               = time.Now()
+		till               = from.AddDate(0, 0, 5)
+		reservation        = fixtures.AddReservation(db.Store, user.ID, car.ID, from, till)
+		app                = fiber.New()
+		route              = app.Group("/", middleware.JWTAuthentication(db.User))
+		reservationHandler = NewReservationHandler(db.Store)
+	)
+
+	route.Get("/:id", reservationHandler.HandleGetReservation)
+	req := httptest.NewRequest("GET", fmt.Sprintf("/%s", reservation.ID.Hex()), nil)
+	req.Header.Add("X-Api-Token", CreateTokenFromUser(user))
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected status 200 but got, %d", resp.StatusCode)
+	}
+	var reservationResp *types.Reservation
+	if err := json.NewDecoder(resp.Body).Decode(&reservationResp); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(reservation.ID, reservationResp.ID) {
+		t.Fatalf("the inserted reservation ID is %s\n but the fetched reservation ID is %s\n", reservation.ID, reservationResp.ID)
+	}
+}
+
 func TestAdminGetReservation(t *testing.T) {
 	db := setup(t)
 	defer db.tearDown(t)
@@ -28,10 +65,10 @@ func TestAdminGetReservation(t *testing.T) {
 		reservation        = fixtures.AddReservation(db.Store, user.ID, car.ID, from, till)
 		app                = fiber.New()
 		admin              = app.Group("/", middleware.JWTAuthentication(db.User), middleware.AdminAuth)
-		ReservationHandler = NewReservationHandler(db.Store)
+		reservationHandler = NewReservationHandler(db.Store)
 	)
 	_ = reservation
-	admin.Get("/", ReservationHandler.HandleGetReservations)
+	admin.Get("/", reservationHandler.HandleGetReservations)
 	req := httptest.NewRequest("GET", "/", nil)
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("X-Api-Token", CreateTokenFromUser(adminUser))
@@ -50,7 +87,7 @@ func TestAdminGetReservation(t *testing.T) {
 		t.Fatal(err)
 	}
 	if len(reservations) != 1 {
-		t.Fatalf("expected at lease 1 reservation but got %d", len(reservations))
+		t.Fatalf("expected at least 1 reservation but got %d", len(reservations))
 	}
 	if !reflect.DeepEqual(reservation.ID, reservations[0].ID) {
 		t.Fatalf("the inserted reservation ID is %s\n but the fetched reservation ID is %s\n", reservation.ID, reservations[0].ID)
