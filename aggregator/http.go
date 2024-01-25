2f6050bb490014055eb464081d4fa19104d2c4ca
ali <ali.assar@skiff.com>
2024-01-25 10:34:21 +0330

    making http latency metric

--- aggregator/http.go
+++ aggregator/http.go
@@ -5,14 +5,17 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/Ali-Assar/car-rental-system/types"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/sirupsen/logrus"
 )
 
 type HTTpMetricHandler struct {
 	reqCounter prometheus.Counter
+	reqLatency prometheus.Histogram
 }
 
 func newHTTPMetricHandler(reqName string) *HTTpMetricHandler {
@@ -20,13 +23,27 @@ func newHTTPMetricHandler(reqName string) *HTTpMetricHandler {
 		Namespace: fmt.Sprintf("http_%s_%s", reqName, "request_counter"),
 		Name:      "aggregator",
 	})
+	reqLatency := promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: fmt.Sprintf("http_%s_%s", reqName, "request_latency"),
+		Name:      "aggregator",
+		Buckets:   []float64{0.1, 0.5, 1},
+	})
 	return &HTTpMetricHandler{
 		reqCounter: reqCounter,
+		reqLatency: reqLatency,
 	}
 }
 
 func (h *HTTpMetricHandler) instrument(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer func(start time.Time) {
+			latency := time.Since(start).Seconds()
+			logrus.WithFields(logrus.Fields{
+				"latency": latency,
+				"request": r.RequestURI,
+			}).Info()
+			h.reqLatency.Observe(latency)
+		}(time.Now())
 		h.reqCounter.Inc()
 		next(w, r)
 	}
